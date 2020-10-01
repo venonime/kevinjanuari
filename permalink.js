a9f6f8e94a3b6b5fb5d5d6f4f6085a78d73421f1
Key <kevinjanuwar@gmail.com>
2020-10-01 16:05:41 +0700

    Add files via upload

---
+++ permalink.js
@@ -0,0 +1,10 @@
+var uri = window.location.toString();
+if (uri.indexOf("&m=1","&m=1") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("?m=1","?m=1") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
